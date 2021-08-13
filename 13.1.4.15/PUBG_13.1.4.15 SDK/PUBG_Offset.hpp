ef9fb012f2e306cb329d748db1ce644e01e3804b
qq1245769786 <1245769786@qq.com>
2021-08-13 19:43:54 +0800

    Updata

---
+++ 13.1.4.15/PUBG_13.1.4.15 SDK/PUBG_Offset.hpp
@@ -0,0 +1,50 @@
+// USceneComponent	Type:struct FRotator
+RelativeRotation	=	0x2C0,//(0xC)
+// USceneComponent	Type:struct FVector
+ComponentVelocity	=	0x384,//(0xC)
+// USkeletalMeshComponent	Type:class UAnimInstance*
+AnimScriptInstance	=	0xCA0,//(0x8)
+// APlayerCameraManager	Type:struct FCameraCacheEntry
+CameraCache	=	0x1150,//(0x5D0)
+// APlayerController	Type:class APlayerCameraManager*
+PlayerCameraManager	=	0x4C0,//(0x8)
+// ACharacter	Type:class USkeletalMeshComponent*
+Mesh	=	0x520,//(0x8)
+// UCharacterMovementComponent	Type:float
+MaxAcceleration	=	0x318,//(0x4)
+// UCharacterMovementComponent	Type:struct FVector
+Acceleration	=	0x3B8,//(0xC)
+// UCharacterMovementComponent	Type:struct FVector
+LastUpdateVelocity	=	0x3E0,//(0xC)
+// Class:FMinimalViewInfo	Type:struct FVector
+Location	=	0x10,//(0xC)
+// Class:FMinimalViewInfo	Type:struct FRotator
+Rotation	=	0x24,//(0xC)
+// Class:FMinimalViewInfo	Type:float
+FOV	=	0x34,//(0x4)
+// ATslCharacter	Type:class UWeaponProcessorComponent*
+WeaponProcessor	=	0xD98,//(0x8)
+// ATslCharacter	Type:int
+LastTeamNum	=	0xEF0,//(0x4)
+// ATslCharacter	Type:struct FString
+CharacterName	=	0x1230,//(0x10)
+// ATslCharacter	Type:float
+GroggyHealth	=	0x1268,//(0x4)
+// ATslCharacter	Type:int
+SpectatedCount	=	0x18C0,//(0x4)
+// ATslCharacter	Type:float
+Health	=	0x1F54,//(0x4)
+// UWeaponProcessorComponent	Type:TArray<class ATslWeapon*>
+EquippedWeapons	=	0x2C8,//(0x10)
+// UWeaponProcessorComponent	Type:struct FWeaponArmInfo
+WeaponArmInfo	=	0x2E8,//(0x5)
+// UWeaponTrajectoryData	Type:struct FWeaponRecoilConfig
+RecoilConfig	=	0x48,//(0xD8)
+// UWeaponTrajectoryData	Type:struct FWeaponTrajectoryConfig
+TrajectoryConfig	=	0x120,//(0x48)
+// ATslWeapon_Trajectory	Type:class UWeaponTrajectoryData*
+WeaponTrajectoryData	=	0xFF8,//(0x8)
+// UDroppedItemInteractionComponent	Type:class UItem*
+Item	=	0x670,//(0x8)
+// UCrowdFollowingComponent	Type:class UCharacterMovementComponent*
+CharacterMovement	=	0x4F0,//(0x8)
---
+++ 13.1.4.17/PUBG_13.1.4.17/PUBG_Offset.hpp
@@ -0,0 +1,50 @@
+// USceneComponent	Type:struct FRotator
+RelativeRotation	=	0x200,//(0xC)
+// USceneComponent	Type:struct FVector
+ComponentVelocity	=	0x20C,//(0xC)
+// USkeletalMeshComponent	Type:class UAnimInstance*
+AnimScriptInstance	=	0xC90,//(0x8)
+// APlayerController	Type:class APlayerCameraManager*
+PlayerCameraManager	=	0x4B8,//(0x8)
+// APlayerCameraManager	Type:struct FCameraCacheEntry
+CameraCache	=	0xA40,//(0x5C0)
+// ACharacter	Type:class USkeletalMeshComponent*
+Mesh	=	0x530,//(0x8)
+// UCharacterMovementComponent	Type:float
+MaxAcceleration	=	0x308,//(0x4)
+// UCharacterMovementComponent	Type:struct FVector
+Acceleration	=	0x3A8,//(0xC)
+// UCharacterMovementComponent	Type:struct FVector
+LastUpdateVelocity	=	0x3D0,//(0xC)
+// Class:FMinimalViewInfo	Type:float
+FOV	=	0x8,//(0x4)
+// Class:FMinimalViewInfo	Type:struct FVector
+Location	=	0x590,//(0xC)
+// Class:FMinimalViewInfo	Type:struct FRotator
+Rotation	=	0x59C,//(0xC)
+// ATslCharacter	Type:int
+SpectatedCount	=	0xD90,//(0x4)
+// ATslCharacter	Type:int
+LastTeamNum	=	0xE28,//(0x4)
+// ATslCharacter	Type:float
+Health	=	0xFC8,//(0x4)
+// ATslCharacter	Type:class UWeaponProcessorComponent*
+WeaponProcessor	=	0x1198,//(0x8)
+// ATslCharacter	Type:float
+GroggyHealth	=	0x17B0,//(0x4)
+// ATslCharacter	Type:struct FString
+CharacterName	=	0x1F70,//(0x10)
+// UWeaponProcessorComponent	Type:TArray<class ATslWeapon*>
+EquippedWeapons	=	0x2B8,//(0x10)
+// UWeaponProcessorComponent	Type:struct FWeaponArmInfo
+WeaponArmInfo	=	0x2D8,//(0x5)
+// UWeaponTrajectoryData	Type:struct FWeaponRecoilConfig
+RecoilConfig	=	0x40,//(0xD8)
+// UWeaponTrajectoryData	Type:struct FWeaponTrajectoryConfig
+TrajectoryConfig	=	0x118,//(0x48)
+// ATslWeapon_Trajectory	Type:class UWeaponTrajectoryData*
+WeaponTrajectoryData	=	0xFF8,//(0x8)
+// UDroppedItemInteractionComponent	Type:class UItem*
+Item	=	0x660,//(0x8)
+// UCrowdFollowingComponent	Type:class UCharacterMovementComponent*
+CharacterMovement	=	0x4E0,//(0x8)
