b555ae0c7613fcdda20052d1b7b76ce7fa1b4cfe
qq1245769786 <1245769786@qq.com>
2021-08-18 15:27:23 +0800

    13.1.5.3

---
+++ 13.1.5.3/PUBG_Offset.hpp
@@ -0,0 +1,50 @@
+// USceneComponent	Type:struct FVector
+ComponentVelocity	=	0x330,//(0xC)
+// USceneComponent	Type:struct FRotator
+RelativeRotation	=	0x33C,//(0xC)
+// APlayerCameraManager	Type:struct FCameraCacheEntry
+CameraCache	=	0x440,//(0x5D0)
+// APlayerController	Type:class APlayerCameraManager*
+PlayerCameraManager	=	0x4B8,//(0x8)
+// ACharacter	Type:class USkeletalMeshComponent*
+Mesh	=	0x560,//(0x8)
+// UCharacterMovementComponent	Type:float
+MaxAcceleration	=	0x318,//(0x4)
+// UCharacterMovementComponent	Type:struct FVector
+Acceleration	=	0x3B8,//(0xC)
+// UCharacterMovementComponent	Type:struct FVector
+LastUpdateVelocity	=	0x3E0,//(0xC)
+// USkeletalMeshComponent	Type:class UAnimInstance*
+AnimScriptInstance	=	0xC90,//(0x8)
+// Class:FMinimalViewInfo	Type:float
+FOV	=	0x0,//(0x4)
+// Class:FMinimalViewInfo	Type:struct FVector
+Location	=	0x59C,//(0xC)
+// Class:FMinimalViewInfo	Type:struct FRotator
+Rotation	=	0x5A8,//(0xC)
+// ATslCharacter	Type:class UWeaponProcessorComponent*
+WeaponProcessor	=	0xE30,//(0x8)
+// ATslCharacter	Type:int
+LastTeamNum	=	0x1790,//(0x4)
+// ATslCharacter	Type:struct FString
+CharacterName	=	0x17B8,//(0x10)
+// ATslCharacter	Type:int
+SpectatedCount	=	0x1958,//(0x4)
+// ATslCharacter	Type:float
+GroggyHealth	=	0x1EB0,//(0x4)
+// ATslCharacter	Type:float
+Health	=	0x1F14,//(0x4)
+// UWeaponProcessorComponent	Type:TArray<class ATslWeapon*>
+EquippedWeapons	=	0x2C8,//(0x10)
+// UWeaponProcessorComponent	Type:struct FWeaponArmInfo
+WeaponArmInfo	=	0x2E8,//(0x5)
+// UWeaponTrajectoryData	Type:struct FWeaponRecoilConfig
+RecoilConfig	=	0x48,//(0xD8)
+// UWeaponTrajectoryData	Type:struct FWeaponTrajectoryConfig
+TrajectoryConfig	=	0x120,//(0x48)
+// ATslWeapon_Trajectory	Type:class UWeaponTrajectoryData*
+WeaponTrajectoryData	=	0xFF0,//(0x8)
+// UDroppedItemInteractionComponent	Type:class UItem*
+Item	=	0x660,//(0x8)
+// UCrowdFollowingComponent	Type:class UCharacterMovementComponent*
+CharacterMovement	=	0x4F0,//(0x8)
